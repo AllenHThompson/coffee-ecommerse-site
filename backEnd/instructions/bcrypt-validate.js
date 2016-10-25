f33807649beb25dc577b152850e7e0189ad46034
Allen H. Thompson <allenhthompson1@gmail.com>
2016-10-25 18:18:07 -0400

    Unintended changes.

--- backEnd/instructions/bcrypt-validate.js
+++ backEnd/instructions/bcrypt-validate.js
@@ -4,13 +4,13 @@ var myPassword = 'opensesame';
 var encryptedPassword = '$2a$10$PJO8bCQFrwUb12DNfqIttO1ugVrPeJQbUHIiYrXXF1rBWtZa//lcK';
 
 bcrypt.compare(myPassword, encryptedPassword, function(err, matched) {
-  if (err) {
-    console.error(err.message);
-    return;
-  }
-  if (matched) {
-    console.log('You are logged in!');
-  } else {
-    console.log('Invalid password.');
-  }
+     if (err) {
+          console.error(err.message);
+          return;
+     }
+     if (matched) {
+          console.log('You are logged in!');
+     } else {
+          console.log('Invalid password.');
+     }
 })
